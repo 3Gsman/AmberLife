c9c514e78e72d7b5fc7f9129b3b82e88ff61f4cd
JavierRelinque <35464459+JavierRelinque@users.noreply.github.com>
2018-02-08 17:04:42 +0100

    Add files via upload

--- src/control/TecnCtrl.java
+++ src/control/TecnCtrl.java
@@ -5,32 +5,49 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.io.IOException;
 
+import javax.swing.JTextField;
+
 import view.*;
 import control.*;
 
 public class TecnCtrl implements ActionListener{
 
 	TecnFr tf;
+	private JTextField textField;
 	
+	public TecnCtrl(TecnFr vm) {
+		tf = vm;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		System.out.println("Action received: ");
+		 if (e.getActionCommand().equals("SEARCH")){
+			System.out.println(" Search");
+			buscarPaciente();
+				 
 		
-		 /*if (e.getActionCommand().equals("LOGOUT")){
-			 System.out.println("Test2");
-			 try {
+			 }else System.out.println(" Null");
+			/* try {
 				volverLogin();
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-		}else System.out.println(" Null");*/
+		}else System.out.println(" Null");
 		
-       /* if (e.getSource().equals(tf.blogout)){
+        if (e.getSource().equals(tf.blogout)){
         			System.out.println("Test1\n");
         }
         
        */
 		
 	}
+	
+	public void buscarPaciente() {
+		System.out.println(tf.getID());
+	}
+	
+	
 		 
 
 
