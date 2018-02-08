7d90af2e1af61034197eeb17b36e90d05f0802b0
JavierRelinque <35464459+JavierRelinque@users.noreply.github.com>
2018-02-08 16:34:04 +0100

    Add files via upload

--- src/control/TecnCtrl.java
+++ src/control/TecnCtrl.java
@@ -1,5 +1,45 @@
 package control;
 
-public class TecnCtrl {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.io.IOException;
 
+import view.*;
+import control.*;
+
+public class TecnCtrl implements ActionListener{
+
+	TecnFr tf;
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		 /*if (e.getActionCommand().equals("LOGOUT")){
+			 System.out.println("Test2");
+			 try {
+				volverLogin();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}else System.out.println(" Null");*/
+		
+       /* if (e.getSource().equals(tf.blogout)){
+        			System.out.println("Test1\n");
+        }
+        
+       */
+		
+	}
+		 
+
+
+	public void volverLogin() throws IOException {
+        tf.dispose();  //Cierra la ventana de tecnico
+        LoginFr login = new LoginFr(); //crea nueva ventana
+        Main mc = new Main (login);    //crea nuevo controlador de ventana
+        login.addController(mc);   //asigna el controlador a la ventana creada
+        login.initialize();
+		
+	}
 }
