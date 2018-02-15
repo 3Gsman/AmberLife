9473b3276707cbf5a40db10b754b07a048a89e98
Germán García <germanpublingo@gmail.com>
2018-02-15 12:56:37 +0100

    Graph system added.

--- src/control/TecnPatientCtrl.java
+++ src/control/TecnPatientCtrl.java
@@ -6,6 +6,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
 
+import view.DoctorFr;
+import view.TecnFr;
+import view.TecnMeasureFr;
+
 public class TecnPatientCtrl implements ActionListener, KeyListener{
 	
 	
@@ -32,7 +36,14 @@ public class TecnPatientCtrl implements ActionListener, KeyListener{
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("Action received: ");
 		 if (e.getActionCommand().equals("MEASURE")){
-			System.out.println("Pantalla ECG");
+			
+			 	//.setVisible(false);	/
+		        TecnMeasureFr tmf = new TecnMeasureFr();
+		        TecnMeasureCtrl tmc = new TecnMeasureCtrl();
+		        tmf.addController(tmc);
+		        //tmf.setVisible(true);
+		        
+			 System.out.println("Pantalla ECG");
 			 }else System.out.println(" Null");
 		
 	}
