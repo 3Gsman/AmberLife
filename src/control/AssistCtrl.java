35f0b04566b1afac587d21c13dd1af045918e935
40CompilerErrors <danvgdesign@gmail.com>
2018-02-26 18:48:57 +0100

    Revert "Revert "Merge branch 'Grafica' of https://github.com/ProyectoInformatica/PIg17-amberlife into Grafica""
    This reverts commit 49ec6bbfa2b6797c19427e94c8aebe85c31d53d1.

--- src/control/AssistCtrl.java
+++ src/control/AssistCtrl.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
-import java.util.Vector;
 
 import javax.swing.JOptionPane;
 
@@ -92,7 +91,6 @@ public class AssistCtrl extends ReturnsToFrame implements ActionListener, KeyLis
 		FileManager id = new FileManager();
 		
 		Patient resultado = id.checkId(dni);
-		Vector<Assistant> test = id.getAssistants();
 		if(resultado.getNumber() != "null") {
 			System.out.println("Patient found.\n");
 			System.out.println("Technician: " + getName());
