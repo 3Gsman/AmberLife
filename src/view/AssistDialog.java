96616cc88a0e504ca90884a8e041678e85dc43d0
JavierRelinque <javierrelinque@hotmail.com>
2018-03-01 17:57:46 +0100

    Password para User y Assit

--- src/view/AssistDialog.java
+++ src/view/AssistDialog.java
@@ -1,23 +1,32 @@
 package view;
 
+import java.util.Arrays;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class AssistDialog {
 	public AssistDialog(JFrame f){
 		JTextField nombre = new JTextField();
 		JTextField lastname = new JTextField();
-		JTextField password = new JTextField();
+		JPasswordField password = new JPasswordField();
+		JPasswordField confirm = new JPasswordField();
 		JTextField id = new JTextField();
 		JTextField city = new JTextField();
 		
 		Object[] inputFields = {"Name:", nombre,
 								"Last Name:", lastname,
 								"Password:", password,
+								"Confirm Password:", confirm,
 								"ID:", id,
 								"City:", city};
 		
 		JOptionPane.showConfirmDialog(f, inputFields, "New Assistant", JOptionPane.OK_CANCEL_OPTION);
+		
+		if(!(Arrays.equals(password.getPassword(), confirm.getPassword()))) {
+			JOptionPane.showMessageDialog(f, "The password doesn't match", "Error", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
--- src/view/DoctorDialog.java
+++ src/view/DoctorDialog.java
@@ -1,7 +1,10 @@
 package view;
 
+import java.util.Arrays;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class DoctorDialog {
@@ -9,7 +12,8 @@ public class DoctorDialog {
 	public DoctorDialog(JFrame f){
 		JTextField nombre = new JTextField();
 		JTextField lastname = new JTextField();
-		JTextField password = new JTextField();
+		JPasswordField password = new JPasswordField();
+		JPasswordField confirm = new JPasswordField();
 		JTextField id = new JTextField();
 		JTextField ssn = new JTextField();
 		JTextField hospital = new JTextField();
@@ -18,11 +22,18 @@ public class DoctorDialog {
 		Object[] inputFields = {"Name:", nombre,
 								"Last Name:", lastname,
 								"Password:", password,
+								"Confirm Password:", confirm,
 								"ID:", id,
 								"SSN:", ssn,
 								"Hospital:", hospital,
 								"Phone:", phone};
 		
 		JOptionPane.showConfirmDialog(f, inputFields, "New Doctor", JOptionPane.OK_CANCEL_OPTION);
+
+		
+		
+		if(!(Arrays.equals(password.getPassword(), confirm.getPassword()))) {
+			JOptionPane.showMessageDialog(f, "The password doesn't match", "Error", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
