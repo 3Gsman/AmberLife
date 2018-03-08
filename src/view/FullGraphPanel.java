ebf04d767467d1b812f79304b9ebd940a06756f5
Germán <germanpublingo@gmail.com>
2018-03-08 18:21:16 +0100

    Graph v1.0

--- src/view/FullGraphPanel.java
+++ src/view/FullGraphPanel.java
@@ -1,6 +1,9 @@
 package view;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.event.ChangeEvent;
@@ -12,6 +15,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
@@ -20,111 +24,198 @@ import org.jfree.data.xy.XYSeriesCollection;
 import model.ECG;
 import model.FileManager;
 
-public class FullGraphPanel extends JPanel implements ChangeListener {
+public class FullGraphPanel extends JPanel implements ChangeListener, ActionListener {
 
-    ECG ECGData;
+	ECG ECGData;
 
-    private static final long serialVersionUID = 1L;
-    FileManager fichero;
+	private static final long serialVersionUID = 1L;
+	FileManager fichero;
 
-    private static int initialValue = 0;
-    private JSlider slider;
-    JFreeChart chart;
-    int lastValue = initialValue;
+	static int initialValue = 0;
+	JSlider slider;
+	JButton bzoomin;
+	JButton bzoomout;
+	JButton reset;
+	JFreeChart chart;
+	int lastValue = initialValue;
 
-    XYSeriesCollection dataset = new XYSeriesCollection();
-    
-    int m;
-    double minimum;
-    double maximum;
-    int t;
-    double frec;
-    double zoom = 5.0;
-    double zoom2 = -1.0;
+	XYSeriesCollection dataset = new XYSeriesCollection();
 
-    public FullGraphPanel(ECG ecgData) {
-        super(new BorderLayout());
-        
-        ECGData = ecgData;
-        frec = ECGData.getFrequency();
-        //m = 1000 / ECGData.getFrequency();//PERIODO
-        t = (ECGData.getFrequency()*ECGData.getData().size());
-        XYDataset dataset = createDataset();
-        chart = createChart(dataset);
-        minimum = (t/100)*-1;
-        maximum = (t/100)*zoom;
-        chart.getXYPlot().getDomainAxis().setRange(minimum, maximum);
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setDomainZoomable(true);
-        chartPanel.setRangeZoomable(true);
+	public int m;
+	public int value;
+	public double minimum;
+	public double maximum;
+	public double t;
+	public double frec;
+	public double frecsec;
+	public double zoom = 3;
+	public double zoom2 = -1 * (zoom);
 
-        add(chartPanel);
+	public FullGraphPanel(ECG ecgData) {
+		super(new BorderLayout());
 
-        JPanel dashboard = new JPanel(new BorderLayout());
-        dashboard.setBorder(BorderFactory.createEmptyBorder(0, 4, 4, 4));
+		ECGData = ecgData;
+		frec = ECGData.getFrequency();
+		frecsec = 1000 / frec;
+		// m = 1000 / ECGData.getFrequency();//PERIODO
+		t = (frecsec * ECGData.getData().size());
+		XYDataset dataset = createDataset();
+		chart = createChart(dataset);
+		minimum = (t / 100) * zoom2;
+		maximum = (t / 100) * zoom;
+		chart.getXYPlot().getDomainAxis().setRange(minimum, maximum);
+		ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setDomainZoomable(true);
+		chartPanel.setRangeZoomable(true);
 
-        this.slider = new JSlider(0,100, 0);
-        this.slider.addChangeListener(this);
-        dashboard.add(this.slider);
-        add(dashboard, BorderLayout.SOUTH);
-    }
+		add(chartPanel);
 
-    public XYDataset createDataset() {
+		JPanel dashboard = new JPanel();
+		GridBagLayout dashboard_gbl = new GridBagLayout();
+		// dashboard_gbl.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 5));
+		dashboard_gbl.columnWidths = new int[] { 20, 120, 20, 20, 0, 0 };
+		dashboard_gbl.rowHeights = new int[] { 30 };
+		dashboard_gbl.columnWeights = new double[] { 0.1, 2.0, 0.1, 0.1, 0.0, 0.0, Double.MIN_VALUE };
+		dashboard_gbl.rowWeights = new double[] { 0.25, Double.MIN_VALUE };
+		dashboard.setLayout(dashboard_gbl);
 
-        final XYSeries series1 = new XYSeries("");
-        final XYSeries series2 = new XYSeries("");
+		dashboard.setBorder(BorderFactory.createEmptyBorder(0, 4, 4, 4));
 
-        Double[] nums = ECGData.getData().toArray(new Double[ECGData.getData().size()]);
+		this.bzoomin = new JButton("+");
+		this.bzoomin.setActionCommand("zoomin");
 
-        int a = 0;
-        for (int i = 0;i < nums.length; i++) {
-            series2.add(frec*i, nums[a]);
-            a++;
-        }
-       
-        dataset.addSeries(series1);
-        dataset.addSeries(series2);
+		this.bzoomout = new JButton("-");
+		this.bzoomout.setActionCommand("zoomout");
+		this.reset = new JButton("Reset");
+		this.reset.setActionCommand("reset");
 
-        return dataset;
+		this.slider = new JSlider(0, 100, 0);
+		this.slider.addChangeListener(this);
+		GridBagConstraints gbc_slider = new GridBagConstraints();
+		gbc_slider.gridwidth = 1;
+		gbc_slider.insets = new Insets(0, 0, 5, 5);
+		gbc_slider.fill = GridBagConstraints.BOTH;
+		gbc_slider.gridx = 1;
+		gbc_slider.gridy = 0;
+		dashboard.add(this.slider, gbc_slider);
 
-    }
+		GridBagConstraints gbc_bzoomout = new GridBagConstraints();
+		gbc_bzoomout.gridwidth = 1;
+		gbc_bzoomout.insets = new Insets(0, 0, 5, 5);
+		gbc_bzoomout.fill = GridBagConstraints.BOTH;
+		gbc_bzoomout.gridx = 2;
+		gbc_bzoomout.gridy = 0;
+		dashboard.add(this.bzoomout, gbc_bzoomout);
 
-    private JFreeChart createChart(final XYDataset dataset) {
+		GridBagConstraints gbc_bzoomin = new GridBagConstraints();
+		gbc_bzoomin.gridwidth = 1;
+		gbc_bzoomin.insets = new Insets(0, 0, 5, 5);
+		gbc_bzoomin.fill = GridBagConstraints.BOTH;
+		gbc_bzoomin.gridx = 3;
+		gbc_bzoomin.gridy = 0;
+		dashboard.add(this.bzoomin, gbc_bzoomin);
 
-        final JFreeChart chart = ChartFactory.createXYLineChart(
-                "", // chart title
-                "", // x axis label
-                "", // y axis label
-                dataset, // data
-                PlotOrientation.VERTICAL,
-                false, // include legend
-                true, // tooltips
-                false // urls
-        );
+		GridBagConstraints gbc_reset = new GridBagConstraints();
+		gbc_reset.gridwidth = 1;
+		gbc_reset.insets = new Insets(0, 0, 5, 5);
+		gbc_reset.fill = GridBagConstraints.BOTH;
+		gbc_reset.gridx = 0;
+		gbc_reset.gridy = 0;
+		dashboard.add(this.reset, gbc_reset);
 
-        chart.setBackgroundPaint(Color.white);
+		bzoomin.addActionListener(this);
+		bzoomout.addActionListener(this);
+		reset.addActionListener(this);
 
-        XYPlot plot = chart.getXYPlot();
-        plot.setBackgroundPaint(Color.darkGray);
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
+		add(dashboard, BorderLayout.SOUTH);
 
-        return chart;
+	}
 
-    }
-    
-    @Override
-    public void stateChanged(ChangeEvent arg0) {
+	public XYDataset createDataset() {
 
-        int value = this.slider.getValue();
-        
-        
-        
-        minimum = (t/100)*(value-zoom2);
-        maximum = (t/100)*(value+zoom);
-        
-        chart.getXYPlot().getDomainAxis().setRange(minimum, maximum);
+		final XYSeries series1 = new XYSeries("");
+		final XYSeries series2 = new XYSeries("");
 
-    }
+		Double[] nums = ECGData.getData().toArray(new Double[ECGData.getData().size()]);
+
+		int a = 0;
+		for (int i = 0; i < nums.length; i++) {
+			series2.add(frecsec * i, nums[a]);
+			a++;
+		}
+
+		dataset.addSeries(series1);
+		dataset.addSeries(series2);
+
+		return dataset;
+
+	}
+
+	private JFreeChart createChart(final XYDataset dataset) {
+
+		final JFreeChart chart = ChartFactory.createXYLineChart("", // chart
+																	// title
+				"", // x axis label
+				"", // y axis label
+				dataset, // data
+				PlotOrientation.VERTICAL, false, // include legend
+				true, // tooltips
+				false// urls
+		);
+
+		chart.setBackgroundPaint(Color.white);
+
+		XYPlot plot = chart.getXYPlot();
+		XYItemRenderer renderer = chart.getXYPlot().getRenderer();
+		renderer.setSeriesPaint(1, new Color(247, 205, 39, 255));
+		plot.setBackgroundPaint(Color.darkGray);
+		plot.setDomainGridlinePaint(Color.white);
+		plot.setRangeGridlinePaint(Color.white);
+
+		return chart;
+
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+
+		value = this.slider.getValue();
+
+		minimum = (t / 100) * (value + zoom2);
+		maximum = (t / 100) * (value + zoom);
+
+		chart.getXYPlot().getDomainAxis().setRange(minimum, maximum);
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("zoomin")) {
+			if (zoom >= -1.0) {
+				zoom = zoom - 1;
+			}
+			System.out.println("zoom in");
+			minimum = (t / 100) * (value + zoom2);
+			maximum = (t / 100) * (value + zoom);
+			chart.getXYPlot().getDomainAxis().setRange(minimum, maximum);
+		}
+
+		if (e.getActionCommand().equals("zoomout")) {
+			// labelRespuesta.setText( "You are not Boo, go away!" );
+			zoom = zoom + 1;
+			System.out.println("zoom out");
+			minimum = (t / 100) * (value + zoom2);
+			maximum = (t / 100) * (value + zoom);
+			chart.getXYPlot().getDomainAxis().setRange(minimum, maximum);
+		}
+
+		if (e.getActionCommand().equals("reset")) {
+
+			zoom = 3;
+
+			minimum = (t / 100) * (value + zoom2);
+			maximum = (t / 100) * (value + zoom);
+			chart.getXYPlot().getDomainAxis().setRange(minimum, maximum);
+		}
+	}
 
 }
