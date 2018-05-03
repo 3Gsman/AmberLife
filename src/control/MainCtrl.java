d4b0af2230c16c5251926c57aaae61cc441b3a3b
JavierRelinque <javierrelinque@hotmail.com>
2018-05-03 20:45:07 +0200

    Inicio añadir base datos
    Establecida conexión. Como aún no ha sido probada del todo, está en comentarios en la clase MainCtrl.java

--- src/control/MainCtrl.java
+++ src/control/MainCtrl.java
@@ -2,6 +2,10 @@ package control;
 
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -15,6 +19,9 @@ import view.dialogs.ExitDialog;
 
 public class MainCtrl implements WindowListener{
 	
+	
+
+	
 	/**
 	 * Creates an initial LoginFr and a controller for it, initializes services, and starts the program.
 	 * 
@@ -24,11 +31,15 @@ public class MainCtrl implements WindowListener{
 	
 	public static MainFr window = new MainFr();
 	
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
     	LocalizationService.initialize();
+    //	String database = "src/resources/BDAmberLife.db";
+   // 	Connection c = null;
 
         try {
-            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName()); 
+       // 	Class.forName("org.sqlite.JDBC");
+      //  	c = DriverManager.getConnection("jdbc:sqlite:" + database);
+        	UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName()); 
             MainCtrl m = new MainCtrl();
 			LoginFr p = new LoginFr();
 			LoginCtrl pc = new LoginCtrl(p);
@@ -38,6 +49,15 @@ public class MainCtrl implements WindowListener{
 			window.initialize();
 			window.setContentPane(p);
 			window.setVisible(true);
+			
+			
+		/*	Statement stmt = null;
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery( "SELECT * FROM User;" );
+			System.out.println(rs.getString("iduser"));
+			System.out.println(rs.getString("iduser"));
+			stmt.close();
+			c.close();*/
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
