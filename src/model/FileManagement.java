6649e3d9178d5d1957f42de4c4c7fd98468b27e0
40CompilerErrors <danvgdesign@gmail.com>
2018-05-07 19:16:51 +0200

    Deprecated FileManager

--- src/model/FileManagement.java
+++ src/model/FileManagement.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Vector;
-
+@Deprecated
 public class FileManagement {
 	
     private BufferedWriter escritor;
